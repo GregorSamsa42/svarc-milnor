e2c1e99d8a0ffd608396a869e708cb2dbf09741e
GregorSamsa42 <geo.kochar@gmail.com>
2022-07-16 09:46:37 +0200

    new changes

--- src/isomaction.lean
+++ src/isomaction.lean
@@ -1,29 +1,31 @@
 import tactic
 import data.real.basic 
 import topology.metric_space.isometry
-
+import topology.metric_space.emetric_space
+import data.set.finite
 import algebra.big_operators
 
 open_locale big_operators 
 
 open classical
+open metric
 
 @[class]
-structure isom_action  (α : Type*) (β : Type*) [monoid α] [metric_space β] extends mul_action α β :=
+structure isom_action  (α : Type*) (β : Type*) [monoid α] [pseudo_metric_space β] extends mul_action α β :=
 (isom : ∀ g : α, isometry (λ x : β , g•x))
 
 /-instance (α : Type*) (β : Type*) [monoid α] [metric_space β] 
   : has_coe (isom_action α β) (mul_action α β)
 := ⟨isom_action.to_mul_action⟩ 
 -/
+open metric
 
-
-lemma isom_of_isom_action {α : Type*} {β : Type*} [monoid α] [metric_space β] 
+lemma isom_of_isom_action {α : Type*} {β : Type*} [monoid α] [pseudo_metric_space β] 
 [isom_action α β] :
 ∀ g : α, isometry (λ x : β , g•x)
 := isom_action.isom
 
-lemma dist_of_isom (α : Type*) (β : Type*) [monoid α] [metric_space β] 
+lemma dist_of_isom (α : Type*) (β : Type*) [monoid α] [pseudo_metric_space β] 
 [isom_action α β] : ∀ g : α, ∀ x y : β, dist x y = dist (g • x) (g • y) :=
 begin
   intros g x y,
@@ -32,8 +34,89 @@ begin
   exact isom_of_isom_action g,
 end
 
+def isom_img {α : Type*} {β : Type*} [monoid α] [pseudo_metric_space β] 
+[isom_action α β] (g: α) (s : set β) :=
+(λ x : β , g•x) '' s
+--{x : β | ∃ y : β, y ∈ s ∧ g•y = x}
 
-example (n:ℕ) (x: finset.range n → ℝ) (l:ℝ)
-  (h: ∀ i, x i ≤ l)
-  : ∑i in range n, x i ≤ n*l 
-:= by library_search 
+-- das hätte ich gerne mit • notation
+
+
+lemma diam_preserved {α : Type*} {β : Type*} [monoid α] [pseudo_metric_space β] 
+[isom_action α β] (s : set β) (g : α) : diam (isom_img g s) = diam s :=
+begin
+rw isom_img,
+apply isometry.diam_image,
+exact isom_action.isom g,
+end
+
+ def set_closed_ball {α : Type*} [pseudo_metric_space α] (s : set α) (ε : ℝ):
+set α :=
+{y : α | ∃ x : α, x ∈ s ∧ dist y x ≤ ε}
+
+lemma self_set_closed_ball {α : Type*} [pseudo_metric_space α] (s : set α) (ε : ℝ) (enonneg: ε ≥ 0) (x : α) :
+x ∈ s → x ∈ set_closed_ball s ε :=
+begin
+intro xs,
+use x,
+split,
+exact xs,
+rw dist_self, exact enonneg,
+end
+
+theorem isom_of_set_closed_ball {α : Type*} {β : Type*} [group α] [pseudo_metric_space β] [isom_action α β]
+ (s : set β) (g : α) (ε : ℝ): isom_img g (set_closed_ball s ε) = set_closed_ball (isom_img g s) ε :=
+begin
+apply subset_antisymm,
+intros x hx, 
+rw set_closed_ball,
+dsimp,
+rcases hx with ⟨y, ⟨⟨z, ⟨hz1, hz2⟩⟩, hy2⟩ ⟩,
+use g • z,
+split,
+use z, split,
+exact hz1,
+dsimp, refl,
+dsimp at hy2,
+rw ← hy2,
+rw ← dist_of_isom, exact hz2,
+-- before here could have had α monoid
+rintros x ⟨y, ⟨⟨ z, ⟨hz1, hz2⟩⟩, hy2⟩⟩,
+rw isom_img,
+use g⁻¹ • x, split,
+rw set_closed_ball, dsimp,
+use z, 
+split, 
+{ exact hz1, },
+dsimp at hz2,
+have inv: z = g⁻¹ • y,
+  {rw ← hz2, rw ← smul_assoc, simp,},
+rw inv,
+rw ← dist_of_isom, exact hy2, dsimp,
+rw smul_smul, 
+simp,
+end
+
+
+theorem diam_of_ball_of_diam {α : Type*} [pseudo_metric_space α] (B : set α) (ε : ℝ) (epos: ε ≥ 0) : diam (set_closed_ball B ε ) ≤ 2*ε + diam B :=
+begin
+  apply diam_le_of_forall_dist_le,
+  linarith [@diam_nonneg _ _ B],
+  rintros x ⟨a, ⟨ha1, ha2 ⟩ ⟩ y ⟨b, ⟨hb1, hb2⟩ ⟩,
+  calc
+    dist x y ≤ dist x a + dist a y  : dist_triangle x a y
+    ...      ≤ dist x a + dist a b + dist b y : begin rw add_assoc, apply add_le_add (le_refl (dist x a)) (dist_triangle a b y), end
+    ...      ≤ ε + ε + dist a b : by sorry
+    ...      ≤ 2*ε + diam B : by sorry
+end
+
+
+def proper_action_set {α : Type*} {β : Type*} [monoid α] [pseudo_metric_space β] 
+  [isom_action α β] (s : set β) : set α := 
+   {g : α | s ∩ (isom_img g s) ≠ ∅}
+
+def translates_cover {α : Type*} {β : Type*} [monoid α] [pseudo_metric_space β] 
+  [isom_action α β] (s : set β) : Prop :=
+  ∀ x : β, x ∈ (⋃ g : α, isom_img g s)
+
+
--- src/svarcmilnor.lean
+++ src/svarcmilnor.lean
@@ -5,6 +5,7 @@ import .cayleygraphs
 import tactic
 import data.real.basic 
 import topology.metric_space.isometry
+import data.list.basic
 
 import algebra.big_operators
 
@@ -24,18 +25,108 @@ begin
     {
       rw [finset.sum_range_succ, add_mul], 
       apply add_le_add,
-      simp,
+      simp ,
       exact hd,
       simp [h d.succ],
     }
 end
 
-theorem metric_svarcmilnor1 {α : Type*} {β : Type*} [group α] (c : ℝ) (b : ℝ) [quasigeodesic_space β c b] 
-  [isom_action α β] (s : set β) (htrans: @translates_cover α β _ _ _ s) (finitediam : metric.bounded s)
-   : α = subgroup.closure (@proper_action_set α _ _ _ _ s) := sorry
+variables {α : Type*} {β : Type*} [inhabited β] [group α] [inhabited α]
+-- L/nat.ceil(L/d)
 
-theorem metric_svarcmilnor2 {α : Type*} {β : Type*} [group α] (c : ℝ) (b : ℝ) [quasigeodesic_space β c b] 
+
+lemma intS  (c : ℝ) (b : ℝ) (cpos: c > 0) (bnonneg: b ≥ 0)  [quasigeodesic_space β c b cpos bnonneg] 
+  [isom_action α β] (s : set β) (finitediam : metric.bounded s) (g : α) (h : α) (x : β) (y : β) (hx: x ∈ isom_img g s)
+  (hy : y ∈ isom_img h s) (hd : dist x y ≤ 2*b) 
+  : ∃ t ∈ (@proper_action_set α _ _ _ _ (set_closed_ball s (2*b))), g*t = h
+   := 
+begin
+use g⁻¹*h,
+split,
+have hy' : y ∈ set_closed_ball (isom_img g s) (2*b),
+  {use x, split, exact hx, rw dist_comm, exact hd,},
+rw ← isom_of_set_closed_ball at hy',
+have hy'' : y ∈ isom_img h (set_closed_ball s (2*b)),
+-- rcases hx with ⟨ z, ⟨ zs, hz ⟩⟩, dsimp at hz,
+rcases hy with ⟨ w, ⟨ ws, hw ⟩⟩, dsimp at hw,
+use w, 
+split,
+{ apply self_set_closed_ball, linarith, exact ws },
+exact hw, rw proper_action_set, dsimp,
+have i1 : g⁻¹ • y ∈ isom_img (g⁻¹ * g) (set_closed_ball s (2 * b)),
+  { apply isom_img_mul, exact hy', },
+simp at i1,
+rw ← isom_img_one at i1,
+have i2 : g⁻¹ • y ∈ isom_img (g⁻¹ * h) (set_closed_ball s (2 * b)),
+  { apply isom_img_mul, exact hy'', },
+apply set.nonempty.ne_empty,
+rw set.nonempty_def,
+use g⁻¹ • y,
+exact ⟨i1, i2⟩,
+exact g, exact x,
+simp,
+end
+
+namespace list
+
+theorem subdiv {L : ℝ} (Lnonneg : L ≥ 0) {d : ℝ} (dpos : d > 0)
+ : ∃ l : list ℝ, l.head = 0 ∧ (l.reverse).head = L ∧ (∀ i : ℕ, (l.inth i)-(l.inth (i-1)) ≤ d ∧ l.inth i ≥ 0 ∧ l.inth i ≤ L) :=
+begin
+sorry
+end
+
+def right_inv_list (g : α) : list α → list α
+| nil := [g]
+| (cons a l) :=  (g* a⁻¹) :: ((a*(l.head)) :: (erasep (λ x, true) (right_inv_list l)))
+
+
+lemma prod_of_inv (g : α) (l : list α) : g = list.prod (right_inv_list g l) :=
+begin
+sorry
+end
+
+theorem metric_svarcmilnor1  (c : ℝ) (b : ℝ) (cpos: c > 0) (bpos: b > 0)  [quasigeodesic_space β c b cpos (le_of_lt bpos)] 
+  [isom_action α β] (s : set β) (htrans: translates_cover α s) (finitediam : metric.bounded s)
+   : α = subgroup.closure (@proper_action_set α _ _ _ _ (set_closed_ball s (2*b))) :=
+begin
+rw generates_iff_subset,
+intro g,
+-- rw mem_closure_iff_finite_prod,
+let x : β, exact default,
+have h : conn_by_quasigeodesic' x (g • x) c b,
+  apply quasigeodesic_space.quasigeodesics x (g • x),
+rcases h with ⟨ L, Lpos, f, qif⟩,
+have harch : ∃ n : ℕ, L ≤ (n : real)*b/c,
+--apply real.archimedean.arch,
+  {sorry},
+cases harch with n hn,
+induction n with d hd,
+
+
+
+-- -- tlist is subdivision of [O,L] with distances smaller than b/c
+
+-- have tplist : ∃ l : list ℝ, l.head = 0 ∧ (l.reverse).head = L ∧ (∀ i : ℕ, (l.inth i)-(l.inth (i-1)) ≤ b/c ∧  l.inth i ≥ 0 ∧ l.inth i ≤ L),
+--   { apply subdiv Lpos (div_pos bpos cpos), },
+-- rcases tplist with ⟨tlist, ⟨h1, ⟨h2,h3 ⟩⟩⟩,
+-- have listint : ∀ r : ℝ, r ∈ tlist → (r ≥ 0 ∧ r ≤ L),
+--   {sorry,},
+
+-- -- xlist is images of tlist under quasigeodesic
+
+-- let xlist := list.pmap (fpi L Lpos f) tlist listint,
+
+-- -- glist are group elements so that g.s contains x
+
+-- let glist := list.map (cover_element htrans) xlist,
+
+end
+
+theorem metric_svarcmilnor2 (c : ℝ) (b : ℝ) (cpos: c > 0) (bpos: b > 0)  [quasigeodesic_space β c b cpos (le_of_lt bpos)] 
   [isom_action α β] (s : set β) (htrans: @translates_cover α β _ _ _ s) (finitediam : metric.bounded s)
-   : ∀ x : β, @is_QI α β (word_metric (@proper_action_set α _ _ _ _ s) (metric_svarcmilnor1 c b s htrans finitediam)) _ (λ g : α, g • x) := sorry
+   : ∀ x : β, @is_QI α β (word_metric (@proper_action_set α _ _ _ _ (set_closed_ball s (2*b))) (metric_svarcmilnor1 c b s htrans finitediam)) _ (λ g : α, g • x) :=
+   begin
+   sorry
+   end
 
 
